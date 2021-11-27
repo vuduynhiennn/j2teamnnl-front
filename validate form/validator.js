2f0d439bf10310161b8711b07ea2d9315404e54a
vuduynhiennn <vuduynhiencute@gmail.com>
2021-11-28 06:57:27 +0700

    add new file

--- validate form/validator.js
+++ validate form/validator.js
@@ -2,10 +2,10 @@ function validator(formSelector) {
 
     function getParent(element, selector) {
         while (element.parentElement) {
-            if (element.parentElement) {
-                if (element.parentElement.matchs(selector)) {
-                    return element.
-                }
+            if (element.parentElement.matches(selector)) {
+                return element.parentElement;
+            } else {
+                element = element.parentElement;
             }
         }
     }
@@ -17,59 +17,97 @@ function validator(formSelector) {
             return value ? undefined : 'Vui lòng nhập trường này';
         },
         email: function (value) {
-            let regex = '/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
+            let regex = '^[a-z][a-z0-9_\.]{5,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}$';
             return regex.test(value) ? undefined : 'Vui lòng nhập email';
+
         },
         min: function (min) {
             return function (value) {
-                return value.length >= min ? undefined : `Vui lòng nhập ít nhất ${min} kí tự`;
+                return value.length >= min ? undefined : `Vui lòng nhập trên ${min} kí tự`;
             }
-            // return value ? undefined : 'Vui lòng nhập trường này';
+        },
+        max: function (max) {
+            return function (value) {
+                return value.length <= max ? undefined : `Vui lòng nhập dưới ${max} kí tự`;
+            }
         }
-    };
-
-
-
+    }
 
     let formElement = document.querySelector(formSelector);
 
     if (formElement) {
-
-        let inputs = formElement.querySelectorAll('[name][rules]');
-
+        var inputs = formElement.querySelectorAll('[name][rules]');
         for (let input of inputs) {
+
             let rules = input.getAttribute('rules').split('|');
 
-            for (rule of rules) {
-                if (rule.includes(':')) {
-                    let ruleInfor = rule.split(':');
-                    rule = ruleInfor[0];
+            for (let rule of rules) {
+
+                let ruleInfo;
+                let isRuleHasValue = (rule.includes(':'));
+
+                if (isRuleHasValue) {
+                    ruleInfo = rule.split(':');
+                    rule = ruleInfo[0];
                 }
 
+                let ruleFunc = validatorRules[rule];
 
-
+                if (isRuleHasValue) {
+                    ruleFunc = ruleFunc(ruleInfo[1]);
+                }
 
                 if (Array.isArray(formRules[input.name])) {
-                    formRules[input.name].push(validatorRules[rule]); 
+                    formRules[input.name].push(ruleFunc);
                 } else {
-                    formRules[input.name] = [validatorRules[rule]];
-                };
+                    formRules[input.name] = ruleFunc;
+                }
             }
 
-            // lắng nghe sự kiện để validate (blur, )
-            input.onblur = handleValidate;            
+            input.onblur = handleValidate;
+            input.oninput = handleClearErr;
         }
 
         function handleValidate(event) {
             let rules = formRules[event.target.name];
-            let errorMessage;
+            let errorMsg;
 
-            rules.find(function (rule) {
-                errorMessage = rule(event.target.value);
-                return errorMessage;
-            });                        
+            // rules.find(function (rule) {
+                errorMsg = rules(event.target.value);
+                // return errorMsg;
+            // });
+
+            if (errorMsg) {
+                let formGroup = getParent(event.target, '.form-group');
+                if(formGroup) {
+                    formGroup.classList.add('invalid');
+                    let formMsg = formGroup.querySelector('.form-message');
+                    formMsg.innerText = errorMsg;
+                }
+            }   
         }
 
-        // console.log(formRules);
+        function handleClearErr(event) {
+            let formGroup = getParent(event.target, '.form-group');
+            if (formGroup.classList.contains('invalid'));
+                formGroup.classList.remove('invalid');
+
+                let formMessage = formGroup.querySelector('.form-message');
+                if (formMessage) {
+                    formMessage.innerText = '';
+                }
+        }
     }
+
+    formElement.onsubmit = function (event) {
+        event.preventDefault();
+        var inputs = formElement.querySelectorAll('[name][rules]');
+        for (let input of inputs) {
+            console.log(input.value);
+            handleValidate({
+                target: input;
+            });
+        }
+    }
+
 }
